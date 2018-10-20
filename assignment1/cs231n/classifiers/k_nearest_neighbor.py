2f970ade9fbba71bbddc611286acb7fa67a63572
mottig <mottigold@gmail.com>
2018-10-20 20:21:20 +0300

    KNN is done

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -124,11 +124,11 @@ class KNearestNeighbor(object):
     #########################################################################
     # sum(x^2 + y^2 + 2xy) == sum(x^2) + sum(y^2) +sum(2xy)
 
-    x_suared = np.sum(X**2, axis=1)
+    x_squared = np.sum(X**2, axis=1)
     train_squared = np.sum(self.X_train**2, axis=1)
     product = 2*X.dot(self.X_train.T)
 
-    dists = np.sqrt(x_suared[:, np.newaxis] + train_squared - product)
+    dists = np.sqrt(x_squared[:, np.newaxis] + train_squared - product)
     #########################################################################
     #                         END OF YOUR CODE                              #
     #########################################################################
@@ -176,96 +176,3 @@ class KNearestNeighbor(object):
 
     return y_pred
 
-
-if __name__ == "__main__":
-
-    import random
-    import numpy as np
-    from data_utils import load_CIFAR10
-    import matplotlib.pyplot as plt
-
-
-
-    # This is a bit of magic to make matplotlib figures appear inline in the notebook
-    # rather than in a new window.
-    plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
-    plt.rcParams['image.interpolation'] = 'nearest'
-    plt.rcParams['image.cmap'] = 'gray'
-
-    # Load the raw CIFAR-10 data.
-    cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
-
-    # Cleaning up variables to prevent loading data multiple times (which may cause memory issue)
-    try:
-        del X_train, y_train
-        del X_test, y_test
-        print('Clear previously loaded data.')
-    except:
-        pass
-
-    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
-
-    # As a sanity check, we print out the size of the training and test data.
-    print('Training data shape: ', X_train.shape)
-    print('Training labels shape: ', y_train.shape)
-    print('Test data shape: ', X_test.shape)
-    print('Test labels shape: ', y_test.shape)
-
-    num_training = 20#5000
-    mask = list(range(num_training))
-    X_train = X_train[mask]
-    y_train = y_train[mask]
-
-    num_test = 5#500
-    mask = list(range(num_test))
-    X_test = X_test[mask]
-    y_test = y_test[mask]
-
-    X_train = np.reshape(X_train, (X_train.shape[0], -1))
-    X_test = np.reshape(X_test, (X_test.shape[0], -1))
-    print(X_train.shape, X_test.shape)
-
-    classifier = KNearestNeighbor()
-    classifier.train(X_train, y_train)
-
-    dists = classifier.compute_distances_two_loops(X_test)
-    # print(dists.shape)
-
-    # Now implement the function predict_labels and run the code below:
-    # We use k = 1 (which is Nearest Neighbor).
-    y_test_pred = classifier.predict_labels(dists, k=1)
-
-    # Compute and print the fraction of correctly predicted examples
-    num_correct = np.sum(y_test_pred == y_test)
-    accuracy = float(num_correct) / num_test
-    print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
-
-    # Now lets speed up distance matrix computation by using partial vectorization
-    # with one loop. Implement the function compute_distances_one_loop and run the
-    # code below:
-    dists_one = classifier.compute_distances_one_loop(X_test)
-
-    # To ensure that our vectorized implementation is correct, we make sure that it
-    # agrees with the naive implementation. There are many ways to decide whether
-    # two matrices are similar; one of the simplest is the Frobenius norm. In case
-    # you haven't seen it before, the Frobenius norm of two matrices is the square
-    # root of the squared sum of differences of all elements; in other words, reshape
-    # the matrices into vectors and compute the Euclidean distance between them.
-    difference = np.linalg.norm(dists - dists_one, ord='fro')
-    print('Difference was: %f' % (difference,))
-    if difference < 0.001:
-        print('Good! The distance matrices are the same')
-    else:
-        print('Uh-oh! The distance matrices are different')
-
-    # Now implement the fully vectorized version inside compute_distances_no_loops
-    # and run the code
-    dists_two = classifier.compute_distances_no_loops(X_test)
-
-    # check that the distance matrix agrees with the one we computed before:
-    difference = np.linalg.norm(dists - dists_two, ord='fro')
-    print('Difference was: %f' % (difference,))
-    if difference < 0.001:
-        print('Good! The distance matrices are the same')
-    else:
-        print('Uh-oh! The distance matrices are different')
